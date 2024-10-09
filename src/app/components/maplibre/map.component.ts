daf7dcae797b72c0a91c46c2f595a0970f5aed09
ace7806 <red7806@gmail.com>
2024-10-08 23:31:25 -0400

    small backend prototype demo

--- src/app/components/maplibre/map.component.ts
+++ src/app/components/maplibre/map.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { NgxMapLibreGLModule, MovingOptions } from '@maplibre/ngx-maplibre-gl';
 import { Map } from 'maplibre-gl';
-import { NgStyle } from '@angular/common';
+import { BackendService } from '../../services/backend.service';
 
 @Component({
   selector: 'app-map',
@@ -10,8 +9,18 @@ import { NgStyle } from '@angular/common';
 })
 
 export class DisplayMapComponent {
-  map: Map; // MapLibre GL Map object (MapLibre is ran outside angular zone, keep that in mind when binding events from this object)
+  constructor(private srvc: BackendService){}
+  gas_stations : Array<any>;
+  // MapLibre GL Map object (MapLibre is ran outside angular zone, keep that in mind when binding events from this object)
+  // unused for now, but very handy reference to the map object
+  map: Map; 
   loadMap(map:Map){
     this.map = map;
+  }
+
+  ngOnInit(): void {
+    this.srvc.getGasStations(-66.5, 18.33, 10).subscribe((response)=> {console.log(response)
+      this.gas_stations = response.features;
+    });
   }
 }
