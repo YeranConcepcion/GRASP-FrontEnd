a3c2591b0b1633cfa7c5f19ec72f2a6c27f0b23f
ace7806 <red7806@gmail.com>
2024-10-25 19:18:44 -0400

    show near gas stations when geolocating

--- src/app/components/maplibre/map.component.ts
+++ src/app/components/maplibre/map.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Map } from 'maplibre-gl';
+import { GeolocateControl, Map } from 'maplibre-gl';
 import { BackendService } from '../../services/backend.service';
 
 @Component({
@@ -9,23 +9,51 @@ import { BackendService } from '../../services/backend.service';
 })
 
 export class DisplayMapComponent {
-  constructor(private srvc: BackendService){}
-  gas_stations : Array<any>;
+  constructor(private srvc: BackendService) { }
+  gas_stations: Array<any>;
+
   // MapLibre GL Map object (MapLibre is ran outside angular zone, keep that in mind when binding events from this object)
   // unused for now, but very handy reference to the map object
-  map: Map; 
-  loadMap(map:Map){
+  map: Map;
+  protected setup(map: Map) {
     this.map = map;
+    this.loadGeolocateControl()
   }
 
-  ngOnInit(): void {
-    this.srvc.getGasStations(-66.5, 18.33, 10).subscribe((response)=> {console.log(response)
-      this.gas_stations = response.features;
+  protected loadGeolocateControl(){
+    let geolocate = new GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true
+    });
+    this.map.addControl(geolocate);
+
+    //automatically locate and show gas stations
+    // setTimeout(() => {
+    //   geolocate.trigger()
+      
+    // }, 3000); 
+    
+    // Set an event listener that fires
+    // when a trackuserlocationend event occurs.
+    geolocate.on('trackuserlocationend', (e) => {
+      console.log('A trackuserlocationend event has occurred.')
+      console.log(e.target._userLocationDotMarker._lngLat.lat)
+      console.log(e.target._userLocationDotMarker._lngLat.lng)
+      this.srvc.getGasStations(e.target._userLocationDotMarker._lngLat.lng, e.target._userLocationDotMarker._lngLat.lat, 4).subscribe((response) => {
+        console.log(response)
+        this.gas_stations = response.features;
+      });
     });
   }
 
-  flyToStation(lng:number, lat:number){
+  ngOnInit(): void {
+    
+  }
+
+  public flyToStation(lng: number, lat: number) {
     if (this.map.loaded() == false) return
-    this.map.flyTo({center: [lng, lat], zoom:17 })
+    this.map.flyTo({ center: [lng, lat], zoom: 17 })
   }
 }
