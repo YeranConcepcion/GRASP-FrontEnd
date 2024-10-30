31a723f530bff76cf00ef79abc1934faf42ab014
YeranConcepcion <yeran.concepcion@upr.edu>
2024-10-29 20:16:18 -0400

    Sign Out implement on navbr dropdown

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { AuthenticatorService } from '@aws-amplify/ui-angular';
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(public authenticator: AuthenticatorService){}
 
 }
