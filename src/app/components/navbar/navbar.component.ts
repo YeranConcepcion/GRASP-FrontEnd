1f720faa76ce3532360f67096fe6cf6fd61fa84c
Ián Colón <ian.colon6@upr.edu>
2024-11-12 14:54:39 -0400

    Fixed Sign-out in profile and about us pages

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthenticatorService } from '@aws-amplify/ui-angular';
+
+
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  constructor(public authenticator: AuthenticatorService){}
+  constructor(
+    public authenticator: AuthenticatorService,
+    private router: Router
+  ) { }
 
+  onSignOut() {
+    this.authenticator.signOut();
+    this.router.navigate(['/'])
+  }
 }
