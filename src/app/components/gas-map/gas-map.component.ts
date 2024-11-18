a8b025985eea56ce67e860c153ab88be7e76b285
ace7806 <red7806@gmail.com>
2024-11-18 00:05:46 -0400

    added some basic filtering - WIP

--- src/app/components/gas-map/gas-map.component.ts
+++ src/app/components/gas-map/gas-map.component.ts
@@ -10,24 +10,30 @@ import { highlightEconomicStations } from '../maplibre/utils';
 export class GasMapComponent {
   constructor(private srvc: BackendService){}
   protected gas_stations: any;
+  protected filtered_gas_stations: any;
+  protected filters : any = {
+    price: 'Lowest',
+    brand: [],
+    gasType: []
+  };
 
 
   ngOnInit(): void {
     this.srvc.getGasStations_aws(-66, 18).subscribe((response) => {
       const data = response;
       data.features = highlightEconomicStations(data.features);
-      console.log("aws");
       this.gas_stations = data;
       console.log(this.gas_stations);
+      this.applyFilters()
     });
   }
   loadAllGasStations(){
     this.srvc.getallGasStations().subscribe((response) => {
       const data = response;
       data.features = highlightEconomicStations(data.features);
-      console.log("aws");
       this.gas_stations = data;
       console.log(this.gas_stations);
+      this.applyFilters()
     });
   }
 
@@ -37,4 +43,67 @@ export class GasMapComponent {
     console.log('gas stations received from child:', this.gas_stations);
   }
 
+   // Handle price filter selection
+   onPriceFilterSelected(order: string) {
+    this.filters.price = order;
+    this.applyFilters();
+  }
+
+  // Handle brand filter selection
+  onBrandFilterToggled(brand: string, isChecked: boolean) {
+    if (isChecked) {
+      this.filters.brand.push(brand);
+    } else {
+      this.filters.brand = this.filters.brand.filter((b:any) => b !== brand);
+    }
+    console.log(brand, this.filters);
+    
+    this.applyFilters();
+  }
+
+  // Handle gas type filter selection
+  onGasTypeFilterToggled(type: string, isChecked: boolean) {
+    if (isChecked) {
+      this.filters.gasType.push(type);
+    } else {
+      this.filters.gasType = this.filters.gasType.filter((t:any) => t !== type);
+    }
+    this.applyFilters();
+  }
+
+  applyFilters() {
+      const tmp = structuredClone(this.gas_stations);
+      let data = tmp.features;
+  
+      // Filter by price
+      if (this.filters.price) {
+        data.sort((a:any, b:any) =>
+          this.filters.price === 'Lowest'
+            ? parseFloat(a.properties.Station_Gas_Price) - parseFloat(b.properties.Station_Gas_Price)
+            : parseFloat(b.properties.Station_Gas_Price) - parseFloat(a.properties.Station_Gas_Price)
+        );
+      }
+  
+      // Filter by brand (using Station_Name to infer brand, adjust as necessary)
+      if (this.filters.brand.length > 0) {
+        data = data.filter((station:any) =>
+          this.filters.brand.some((brand:any) => station.properties.Station_Name.includes(brand))
+        );
+      }
+  
+      // Filter by gas type (this may need adjustments if a gas type is added to the data schema)
+      if (this.filters.gasType.length > 0) {
+        // Assuming future gas type information is part of `properties` (adjust if necessary)
+        data = data.filter((station:any) =>
+          this.filters.gasType.includes(station.properties.Gas_Type) // Placeholder, update based on actual field
+        );
+      }
+  
+      // Exclude stations with invalid prices
+      data = data.filter((station:any) => parseFloat(station.properties.Station_Gas_Price) > 0);
+      tmp.features = data;
+      this.filtered_gas_stations = tmp;
+      console.log(this.gas_stations,this.filtered_gas_stations);
+  }
+  
 }
