5a8a28cab011ab7880566b5c4adcbdba541a0943
ace7806 <red7806@gmail.com>
2024-11-17 22:57:45 -0400

    load gas stations with aws lambdas

--- src/app/components/gas-map/gas-map.component.ts
+++ src/app/components/gas-map/gas-map.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BackendService } from '../../services/backend.service';
+import { highlightEconomicStations } from '../maplibre/utils';
 
 @Component({
   selector: 'app-gas-map',
@@ -6,12 +8,33 @@ import { Component } from '@angular/core';
   styleUrl: './gas-map.component.css'
 })
 export class GasMapComponent {
-  protected receivedList: any;
+  constructor(private srvc: BackendService){}
+  protected gas_stations: any;
+
+
+  ngOnInit(): void {
+    this.srvc.getGasStations_aws(-66, 18).subscribe((response) => {
+      const data = response;
+      data.features = highlightEconomicStations(data.features);
+      console.log("aws");
+      this.gas_stations = data;
+      console.log(this.gas_stations);
+    });
+  }
+  loadAllGasStations(){
+    this.srvc.getallGasStations().subscribe((response) => {
+      const data = response;
+      data.features = highlightEconomicStations(data.features);
+      console.log("aws");
+      this.gas_stations = data;
+      console.log(this.gas_stations);
+    });
+  }
 
   // Method to handle the list emitted from the child
   receiveListFromChild(list: any) {
-    this.receivedList = list;
-    console.log('gas stations received from child:', this.receivedList);
+    this.gas_stations = {...list};
+    console.log('gas stations received from child:', this.gas_stations);
   }
 
 }
