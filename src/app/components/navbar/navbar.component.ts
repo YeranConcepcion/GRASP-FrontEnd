0d6e7e87b5e7338a3db62f8eafacb6b111e987a5
YeranConcepcion <yeran.concepcion@upr.edu>
2024-12-04 12:40:10 -0400

    Updated profile modal

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,7 +3,7 @@ import { Router } from '@angular/router';
 import { AuthenticatorService } from '@aws-amplify/ui-angular';
 import { Dialog, DialogModule } from '@angular/cdk/dialog';
 import { MyProfileComponent } from '../my-profile/my-profile.component';
-
+import { getCurrentUser, fetchUserAttributes } from 'aws-amplify/auth';
 
 
 @Component({
@@ -12,23 +12,38 @@ import { MyProfileComponent } from '../my-profile/my-profile.component';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  userInfo: any = {
+    username: '',
+    email: '',
+    email_verified: '',
+    sub: ''
+  };
   constructor(
     public authenticator: AuthenticatorService,
     private router: Router,
     public dialog: Dialog
   ) { }
+  ngOnInit(): void {
+    this.fetchUserInfo();
+  }
 
   onSignOut() {
     this.authenticator.signOut();
     this.router.navigate(['/'])
   }
+  async fetchUserInfo() {
+    try {
+      const user = await getCurrentUser();
+      this.userInfo.username = user.username;
 
-  openProfileModal(): void {
-    console.log("Opening profile modal...");
-    this.dialog.open(MyProfileComponent, {
-      width: '400px', // Adjust as needed
-      height: '300px', // Adjust as needed
-    });
+      const attributes = await fetchUserAttributes();
+      this.userInfo.email = attributes.email || 'Email unavailable';
+      this.userInfo.email_verified = attributes.email_verified;
+    } catch (error) {
+      console.error('Error fetching user info:', error);
+    }
   }
+
+
 
 }
