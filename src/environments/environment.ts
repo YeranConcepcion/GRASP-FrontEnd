aeb3a85ad6b8950d8e33181c739684f98962a97c
ace7806 <red7806@gmail.com>
2024-12-06 17:21:06 -0400

    updated environment urls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,13 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:3000/api',  // Your development API URL
-    cognitoConfig: {
-      userPoolId: 'us-west-2_UR2oBa8bZ',
-      clientId: '1hh20kasovioaq9mrsqo5fibui'
-    },
-    GETGASSTATIONS : "https://tlvsiwvpgb.execute-api.us-west-2.amazonaws.com/NP/getgasstations",
-    UPDATEGASPRICE: "https://tlvsiwvpgb.execute-api.us-west-2.amazonaws.com/NP/updategasprice",
-    GETCLOSESTGASSTATION: "https://7x2f8ah9ef.execute-api.us-west-2.amazonaws.com/NP/getcloseststation",
-    GETADMINS: "https://tlvsiwvpgb.execute-api.us-west-2.amazonaws.com/NP/getadmins",
-    UPDATERATINGS: "https://7x2f8ah9ef.execute-api.us-west-2.amazonaws.com/NP/stations/rating"
-  };
-  
+  production: false,
+  apiUrl: 'http://localhost:3000/api',  // Your development API URL
+  cognitoConfig: {
+    userPoolId: 'us-west-2_b55x8lSnp',
+    clientId: '1g03nnedl1kt6mkhg4kqu9843r'
+  },
+  GETGASSTATIONS : "https://7x2f8ah9ef.execute-api.us-west-2.amazonaws.com/NP/getgasstations",
+  UPDATEGASPRICE: "https://7x2f8ah9ef.execute-api.us-west-2.amazonaws.com/NP/updategasprice",
+  GETCLOSESTGASSTATION: "https://7x2f8ah9ef.execute-api.us-west-2.amazonaws.com/NP/getcloseststation",
+  GETADMINS: "https://7x2f8ah9ef.execute-api.us-west-2.amazonaws.com/NP/getadmins",
+  UPDATERATINGS: "https://7x2f8ah9ef.execute-api.us-west-2.amazonaws.com/NP/stations/rating"
+};
